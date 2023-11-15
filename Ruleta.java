3a85e8fa52b9c3ed7b90ce68abdb44cba87144b5
smanrique <37903095+smanrique@users.noreply.github.com>
2023-11-15 20:44:38 +0100

    Update Ruleta.java

--- Ruleta.java
+++ Ruleta.java
@@ -1,15 +1,32 @@
+//Creado por Sebastian Manrique e Ignacio Delgado
 import java.util.Scanner;
 public class Ruleta {
 	static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int j1=0;  //Valors de jugadores y solicitud de turno
+        int j1=0;  //Valores de jugadores y solicitud de turno
         int j2=0;  //La variable jugador es el dinero
         int j3=0;  //Su suma de dinero será el producto de multiplicar el número aleatorio en el que haya caído multiplicado
         //Turno jugador 1, gira ruleta, pide letra (bucle), panel, termina turno.
     }
     //paneles tambien
     private static void ruleta() { /*numero aleatorio, los valores en los
-    que puede caer la ruleta son 0, 25, 50, 75, 100, 150 o la quiebra.*/
+	    que puede caer la ruleta son 0, 25, 50, 75, 100, 150 o la quiebra.*/
+	System.out.println("              @@@@@@@@@@@@@             \r\n" //Dibujo de la ruleta hecho.
+			+ "         @@     @@@@@@@@@    ,@@        \r\n"
+			+ "      /@   @@  @@   @   @@ ,@@   @      \r\n"
+			+ "     @  %@ @@   @*  @  @@   @@ @*  @    \r\n"
+			+ "   @@  @@    @@  @@@@@@@  @@    @@  @@  \r\n"
+			+ "  @@  @   ,@@ @@         @@ @@    @  @% \r\n"
+			+ "  @  @@@@@   @  @ @@ @# @  @   @@@@@  @ \r\n"
+			+ "  @  @      @@      @      @@      @  @ \r\n"
+			+ "  @  @@@@@   @  @ @@ @# @  @   @@@@@  @ \r\n"
+			+ "  &@  @    @@ @@         @@ @@    @  @/ \r\n"
+			+ "   @@  @@    @@  @@@@@@@  @@    @@  @@  \r\n"
+			+ "    .@  (@ @@   @/  @  @@   @@ @.  @    \r\n"
+			+ "      @@   @@  @@   @   @@ .@@   @@     \r\n"
+			+ "         @@.    @@@@@@@@@    #@@        \r\n"
+			+ "              @@@@@@@@@@@@@             "
+			+ "\n              Gira la ruleta 🎰");
     
     }
     private static void turno() { // El turno sera un do while
