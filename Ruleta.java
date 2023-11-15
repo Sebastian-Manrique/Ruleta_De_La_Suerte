af6bee9b1b54714daecde4655b610408d083f128
IgnacioDrako <150959756+IgnacioDrako@users.noreply.github.com>
2023-11-15 20:26:00 +0100

    Create Ruleta.java

---
+++ Ruleta.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ruleta {
+	static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int j1=0;  //Valors de jugadores y solicitud de turno
+        int j2=0;  //La variable jugador es el dinero
+        int j3=0;  //Su suma de dinero será el producto de multiplicar el número aleatorio en el que haya caído multiplicado
+        //Turno jugador 1, gira ruleta, pide letra (bucle), panel, termina turno.
+    }
+    //paneles tambien
+    private static void ruleta() { //numero aleatorio
+    
+    }
+    private static void turno() { // El turno sera un do while
+        
+    }
+    private static void panel() { //Panel de palabras
+        
+    }
+    
+}
