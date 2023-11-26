63a1b2cdb742324e38c4298c6092ebd483f24f34
smanrique <37903095+smanrique@users.noreply.github.com>
2023-11-26 19:08:43 +0100

    Penultima version yo creo

--- Ruleta.java
+++ Ruleta.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TurnoY_Quiebra {
+public class RuletaDeLaSuerte {
 	static StringBuilder resultado = new StringBuilder();
 	public static String reemplazarLetras(String texto) {
         resultado = new StringBuilder();
@@ -23,35 +23,40 @@ public class TurnoY_Quiebra {
             Puede aceptar diferentes tipos de datos como parámetro y los convierte automáticamente
             a cadenas de caracteres para su concatenación.*/
         }
-        
         return resultado.toString(); //Lo comvierte a String
     }
 
     static final List<Integer> RESULTADOS = Arrays.asList(0, 25, 50, 75, 100, 150);//Probabilidades de la ruleta
-    static final StringBuilder FRASE = resultado; //Pase la frase a asteriscos
+    static StringBuilder FRASE = resultado; //Pase la frase a asteriscos
     static final int DINERO_INICIAL = 100; //Dinero principal de los jugadores
     static int[] dineroJugadores = new int[3]; //3 variables de dinero para 3 jugadores 
     static int turnoActual = 0; //Establece el turno de los jugadores, se va sumando, empieza en 0.
-
+    static String panel = "";
+    static String pista= "";
+    static int panelNum=0;
     public static void main(String[] args) { //main
         inicializarJuego(); //Mecanica de turnos
         Scanner scanner = new Scanner(System.in);
-        
-        String input = "Nunca llueve a gusto de todos"; //el panel 
-        System.out.println("El panel se mostrara aqui");
+        panelActual();
+         //el panel
 
         // Llamada a la función para reemplazar letras por asteriscos
-        String resultadoTexto = reemplazarLetras(input); // Cambio de nombre de la variable local
-        FRASE.replace(0, FRASE.length(), resultadoTexto); // Reemplaza el contenido de FRASE con resultadoTexto
-        System.out.println("Texto del panel  " + resultadoTexto +"\npista: un refran (La primera mayuscula)."); //Imprime el panel en * y su pista
-        while (!hayGanador()) { //El juego en si
-        	if (turnoActual == 4) {
-        		turnoActual = 0;
-        	}
-            System.out.println("Turno del jugador " + (turnoActual + 1)); //Imprime a que jugador le toca
-            System.out.println("Frase: " + FRASE); //Imprime la frase
+       
+        while (!hayGanador()) { // El juego en sí
+            if (turnoActual == 4) {
+                turnoActual = 0;
+            }
+            panelActual();
+            
+            // Mover la línea dentro del bucle while
+            String resultadoTexto = reemplazarLetras(panel); // Cambio de nombre de la variable local
+            
+            System.out.println("Turno del jugador " + (turnoActual + 1)); // Imprime a qué jugador le toca
+            System.out.println("Frase: " + resultadoTexto + " pista: " + pista); // Imprime la frase
 
-            int resultadoRuleta = girarRuleta();// inicia mecanica de "ruleta"
+            int resultadoRuleta = girarRuleta(); // Inicia mecánica de "ruleta"
+            System.out.println(" ");
+            System.out.println(" ");
             System.out.println(""
             		+ "                                                ..:-=+====--::.                                                   \r\n"//Dibujo de la ruleta
             		+ "                                          .-=++++=-:::**@+#---=++++-:                                             \r\n"
@@ -91,66 +96,65 @@ public class TurnoY_Quiebra {
             		+ "                                       .-+++=:.         @        :-++*=.                                          \r\n"
             		+ "                                            .-=++++++===@==++++++=:                                               \r\n"
             		+ "                                                     .                                                            \r\n"
-            		+ "\nLa ruleta ha caído en: " + resultadoRuleta);
+            		+ "\nLa ruleta ha caido en: " + resultadoRuleta);
 
-            if (resultadoRuleta == 0) { //Si cae un quiebra
+            if (resultadoRuleta == 0) { // Si cae un quiebra
                 System.out.println("¡Quiebra! El jugador pierde todo el dinero ganado.");
-                dineroJugadores[turnoActual] = 0; //camvia turno
+                dineroJugadores[turnoActual] = 0; // cambia turno
+                siguienteTurno();
             } else {
-                boolean adivinoCorrectamente = false; //inicia turno 
+                boolean adivinoCorrectamente = false; // inicia turno 
                 do {
-                    System.out.println("Que quieres hacer: 'resolver frase' o 'decir letra' (escriba la opción)."); //Imprime las opciones
-                    String decision =scanner.nextLine();
-                    if(decision.equals("resolver frase") || decision.equals("resolver") || decision.equals("frase")){//Resolver frase
-                    	System.out.println("El panel es:" +FRASE+" y la pista es un refran");
-                    	String respuesta =scanner.nextLine();
-                    	  if (respuesta.equals(input)) { //verifica la frase
-                    		System.out.println("Enhorabuena, ¡has acertado!");
-                    		hayGanador();
-                    		System.exit(0);
-                    		//termina el juego
-                    	}
-                    	else{
-                    		System.out.println("No has acertado.");
-                    		siguienteTurno();
-                    	}
-                    }
-                    else if(decision.equals("decir letra") || decision.equals("decir") || decision.equals("letra")) {//Decir letra
-                    	System.out.println("Dime la letra a poner.");
-                    	char letra = scanner.nextLine().charAt(0);
-                    	if (input.toUpperCase().indexOf(Character.toUpperCase(letra)) != -1) { //verifica la letra
-                    	    int index = -1;
-                    	    do {
-                    	        index = input.toUpperCase().indexOf(Character.toUpperCase(letra), index + 1);//revela la o las letras
-                    	        if (index != -1) {
-                    	            FRASE.setCharAt(index, letra);
-                    	            dineroJugadores[turnoActual] += resultadoRuleta;
-                    	        }
-                    	    } while (index != -1);
+                    System.out.println("Que quieres hacer: 'resolver frase' o 'decir letra' (escriba la opción)."); // Imprime las opciones
+                    String decision = scanner.nextLine();
+                    if (decision.equals("resolver frase") || decision.equals("resolver") || decision.equals("frase")) {// Resolver frase
+                        System.out.println("El panel es:" + resultadoTexto + " y la pista es: " + pista);
+                        String respuesta = scanner.nextLine();
+                        if (respuesta.equals(panel)) { // verifica la frase
+                            System.out.println("Enhorabuena, ¡has acertado el panel! " + panel);
+                            hayGanador();
+                            panelNum++;
+                            panelActual();
+                        } else {
+                            System.out.println("No has acertado.");
+                            siguienteTurno();
+                        }
+                    } else if (decision.equals("decir letra") || decision.equals("decir") || decision.equals("letra")) {// Decir letra
+                        System.out.println("Dime la letra a poner.");
+                        char letra = scanner.nextLine().charAt(0);
+                        if (panel.toUpperCase().indexOf(Character.toUpperCase(letra)) != -1) { // verifica la letra
+                            int index = -1;
+                            do {
+                                index = panel.toUpperCase().indexOf(Character.toUpperCase(letra), index + 1);// revela la o las letras
+                                if (index != -1) {
+                                    FRASE.setCharAt(index, letra);
+                                    dineroJugadores[turnoActual] += resultadoRuleta;
+                                }
+                            } while (index != -1);
 
-                    	    adivinoCorrectamente = true; //sigue en el turno
-                    	    System.out.println("¡Letra correcta! El jugador gana " + resultadoRuleta + " monedas.");
-                    	    System.out.println("¡Felicidades! Has adivinado la letra: " + FRASE);
-                    	}
-                    	else {
-                    	    System.out.println("Letra incorrecta. El jugador pierde su turno.");
-                    	    adivinoCorrectamente = false; // salta turno 
-                    	}
-                    }else{
-                    	System.out.println("Error.");
+                            adivinoCorrectamente = true; // sigue en el turno
+                            System.out.println("¡Letra correcta! El jugador gana " + resultadoRuleta + " monedas.");
+                            System.out.println("¡Felicidades! Has adivinado la letra: " + panel);
+                        } else {
+                            System.out.println("Letra incorrecta. El jugador pierde su turno.");
+                            adivinoCorrectamente = false; // salta turno 
+                        }
+                    } else {
+                        System.out.println("Error.");
                     }
 
                 } while (adivinoCorrectamente && FRASE.indexOf("*") != -1); // Continuar mientras adivine correctamente y la frase no esté completamente revelada
-            }
 
-            if (FRASE.indexOf("*") == -1) {
-                System.out.println("¡Felicidades! Has adivinado la frase: " + input);
-                break;
-                //Termina el juego
-            }
+                if (FRASE.indexOf("*") == -1) {
+                    System.out.println("¡Felicidades! Has adivinado la frase: " + panel);
+                    panelActual();
+                    // Termina el juego
+                }
 
-            siguienteTurno();
+                siguienteTurno();
+            }
         }
+
     }
 
     static void inicializarJuego() {    //Ejecuta el juego
@@ -167,6 +171,32 @@ public class TurnoY_Quiebra {
     static void siguienteTurno() {	//Coge la variable turno y la va sumando
         turnoActual = (turnoActual + 1) % dineroJugadores.length;
     }
+    static void panelActual() {
+    	
+        switch (panelNum) {
+            case 0:
+                panel = "yo era un aventurero";
+                pista = "Algunos soldados les da nostalgia verte.";
+                break;
+            case 1:
+                panel = "nunca llueve a gusto de todos";
+                pista = "Lo que resulta placentero para unos puede desagradar a otros";
+                break;
+            case 2:
+                panel = "victoria o muerte";
+                pista = "Dalo todo hasta morir";
+                break;
+            default:
+                System.out.println("Fin del juego");
+                System.exit(0); //Apaga el juego.
+                
+                if (panelNum >= 3) {
+                    panelNum=0; // Reinicia panelNum cuando llega al último panel
+            }
+          }
+        }
+        
+        
 
     static void mostrarDineroJugadores() {			//Como su nombre indica, imprime el dinero de los juegadores
         for (int i = 0; i < dineroJugadores.length; i++) {
